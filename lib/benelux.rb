1c9b8fa000e867cd9d96bb4d4e76fce0149f8598
delano <delano@solutious.com>
2009-09-19 14:28:07 -0400

    Pas de Benelux::Object, c'est juste Benelux

--- lib/benelux.rb
+++ lib/benelux.rb
@@ -1,63 +1,51 @@
 require 'attic'
 
 module Benelux
-  extend self
-  
-  class Mark < Time
-    attr_accessor :name
-    def self.now(name=nil)
-      v = super()
-      v.name = name
-      v
+
+  def benelux_at(*names)
+    name = Benelux.name *names
+    self.benelux_timeline.select do |mark| 
+      mark.name.to_s == name.to_s
     end
   end
+  
+  def benelux_between(*names)
+    nameA, nameB = *names.collect { |n| Benelux.name n }
+    timeA, timeB = benelux_at(nameA), benelux_at(nameB)
+    timeB.first.to_f - timeA.first.to_f
+  end
+  
+  def benelux_mark(*names)
+    name = Benelux.name *names
+    self.benelux_timeline ||= []
+    self.benelux_timeline << Benelux::Mark.now(name)
+  end
+  private :benelux_mark
   
   @@timed_objects = []
-  def add_timer obj, meth
-    meth_alias = "__benelux_#{meth}_#{Thread.current.object_id.abs}_#{obj.object_id.abs}"
-    obj.extend Attic
-    obj.attic :benelux_timeline
-    obj.module_eval do
-      alias_method meth_alias, meth
-    end
-    obj.module_eval benelux_methods_str(meth_alias, meth)
-    timed_objects << obj
-  end
   
-  def timed_objects
+  def Benelux.timed_objects
     @@timed_objects
   end
   
-  def name(*names)
+  def Benelux.name(*names)
     names.flatten.collect { |n| n.to_s }.join('_')
   end
   
-  private
-  
-  def benelux_methods_str(meth_alias, meth)
-    %Q{
-    unless respond_to?(:__benelux_mark)
-      
-      def benelux_at(*names)
-        name = Benelux.name *names
-        self.benelux_timeline.select do |mark| 
-          mark.name.to_s == name.to_s
-        end
-      end
-      
-      def benelux_between(*names)
-        nameA, nameB = *names.collect { |n| Benelux.name n }
-        timeA, timeB = benelux_at(nameA), benelux_at(nameB)
-        timeB.first.to_f - timeA.first.to_f
-      end
-      
-      def benelux_mark(*names)
-        name = Benelux.name *names
-        self.benelux_timeline ||= []
-        self.benelux_timeline << Benelux::Mark.now(name)
-      end
-      private :benelux_mark
+  def Benelux.add_timer obj, meth
+    meth_alias = "__benelux_#{meth}_#{Thread.current.object_id.abs}_#{obj.object_id.abs}"
+    obj.extend Attic
+    obj.attic :benelux_timeline
+    obj.send :include, Benelux
+    obj.module_eval do
+      alias_method meth_alias, meth
     end
+    obj.module_eval generate_wrapper_str(meth_alias, meth)
+    timed_objects << obj unless timed_objects.member? obj
+  end
+  
+  def Benelux.generate_wrapper_str(meth_alias, meth)
+    %Q{
     def #{meth}(*args, &block)  
       benelux_mark(:'#{meth}', :start)
       ret = #{meth_alias}(*args, &block)
@@ -69,3 +57,13 @@ module Benelux
   
 end
 
+module Benelux
+  class Mark < Time
+    attr_accessor :name
+    def self.now(name=nil)
+      v = super()
+      v.name = name
+      v
+    end
+  end
+end
