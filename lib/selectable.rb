55a92f4b30f176e281e3fe906f9efcabfaee5e64
delano <delano@solutious.com>
2009-10-04 14:48:13 -0400

    Selectable refactor

--- lib/selectable.rb
+++ lib/selectable.rb
@@ -13,9 +13,18 @@ module Selectable
   class SelectableError < RuntimeError; end
   class TagsNotInitialized < SelectableError; end
   
+  require 'selectable/tags'
+  require 'selectable/object'
+  
+  # Returns a Hash or Array
   def Selectable.normalize(*tags)
     tags.flatten!
     tags = tags.first if tags.first.kind_of?(Hash) || tags.first.kind_of?(Array)
+    if tags.is_a?(Hash)
+      tags = Hash[tags.collect { |n,v| [n, v.to_s] }]
+    else
+      tags.collect! { |v| v.to_s }
+    end
     tags
   end
   
@@ -40,130 +49,6 @@ module Selectable
   def filter!(*tags)
     tags = Selectable.normalize tags
     self.delete_if { |obj|   obj.tags < tags }
-  end
-  
-  # Helper methods for objects with a @tags instance var
-  #
-  # e.g. 
-  #
-  #     class Something
-  #       include Selectable::Object
-  #     end
-  #
-  module Object
-    attr_accessor :tags
-    def add_tags(tags)
-      init_tags
-      @tags.merge! tags
-    end
-    alias_method :add_tag, :add_tags
-    def remove_tags(*tags)
-      raise TagsNotInitialized if @tags.nil?
-      tags.flatten!
-      @tags.delete_if { |n,v| tags.member?(n) }
-    end
-    alias_method :remove_tag, :remove_tags
-    def tag_values(*tags)
-      raise TagsNotInitialized if @tags.nil?
-      tags.flatten!
-      ret = @tags.collect { |n,v| 
-        v if tags.empty? || tags.member?(n) 
-      }.compact
-      ret
-    end
-    def self.normalize(tags={})
-      tags = tags.first if tags.kind_of?(Array) && tags.first.kind_of?(Hash)
-      tags = [tags].flatten unless tags.kind_of?(Hash)
-      tags
-    end
-    def init_tags
-      @tags ||= Selectable::Tags.new
-    end
-  end
-  
-  # An example of filtering an Array of tagged objects based
-  # on a provided Hash of tags or Array of tag values. +obj+
-  # in this case would be an object that includes Taggable.
-  #
-  #     class Something
-  #       def [](tags={})
-  #         tags = [tags].flatten unless tags.is_a?(Hash)
-  #         self.select do |obj|
-  #           obj.tags >= tags
-  #         end
-  #       end
-  #     end
-  #
-  class Tags < ::Hash
-    
-    def to_s
-      tagstr = []
-      self.each_pair do |n,v|
-        tagstr << "%s=%s" % [n,v]
-      end
-      tagstr.join ' '
-    end
-    
-    def inspect
-      to_s
-    end
-    
-    def ==(other)
-      if other.is_a?(Array)
-        (self.values & other).sort == other.sort
-      else
-        super(other)
-      end
-    end
-    
-    # Comparison between other Hash and Array objects.
-    #
-    # e.g.
-    #
-    #     a = {:a => 1, :b => 2}
-    #     a > {:a => 1, :b => 2, :c => 3}    # => false
-    #     a > {:a => 1}                      # => true
-    #     a < {:a => 1, :b => 2, :c => 3}    # => true
-    #     a >= [2, 1]                        # => true
-    #     a > [2, 1]                         # => false
-    #
-    def <=>(b)
-      return 0 if self == b
-      self.send :"compare_#{b.class}", b
-    end
-        
-    def >(other)  (self <=> other)  > 0 end
-    def <(other)  (self <=> other)  < 0 end
-    
-    def <=(other) (self <=> other) <= 0 end
-    def >=(other) (self <=> other) >= 0 end
-    
-    private
-    
-    def compare_Hash(b)
-      a = self
-      return -1 unless (a.values_at(*b.keys) & b.values).size >= b.size
-      1
-    end
-    alias_method :"compare_Selectable::Tags", :compare_Hash
-    
-    def compare_Array(b)
-      return -1 unless (self.values & b).size >= b.size
-      1
-    end
-    
-    def method_missing(meth, *args)
-      raise SelectableError, "#{args.first} is not a Hash or Array"
-    end
-    
-    ## NOTE: This is helpful but defensive. Ponder!
-    ##def compare_forced_array(b)
-    ##  compare_Array([b])
-    ##end
-    ##alias_method :compare_String, :compare_forced_array
-    ##alias_method :compare_Symbol, :compare_forced_array
-    ##alias_method :compare_Fixnum, :compare_forced_array
-      
   end
 
 end
---
+++ lib/selectable/tags.rb
@@ -0,0 +1,90 @@
+
+
+module Selectable
+  
+  # An example of filtering an Array of tagged objects based
+  # on a provided Hash of tags or Array of tag values. +obj+
+  # in this case would be an object that includes Taggable.
+  #
+  #     class Something
+  #       def [](tags={})
+  #         tags = [tags].flatten unless tags.is_a?(Hash)
+  #         self.select do |obj|
+  #           obj.tags >= tags
+  #         end
+  #       end
+  #     end
+  #
+  class Tags < ::Hash
+    
+    def to_s
+      tagstr = []
+      self.each_pair do |n,v|
+        tagstr << "%s=%s" % [n,v]
+      end
+      tagstr.join ' '
+    end
+    
+    def inspect
+      to_s
+    end
+    
+    def ==(other)
+      if other.is_a?(Array)
+        (self.values & other).sort == other.sort
+      else
+        super(other)
+      end
+    end
+    
+    # Comparison between other Hash and Array objects.
+    #
+    # e.g.
+    #
+    #     a = {:a => 1, :b => 2}
+    #     a > {:a => 1, :b => 2, :c => 3}    # => false
+    #     a > {:a => 1}                      # => true
+    #     a < {:a => 1, :b => 2, :c => 3}    # => true
+    #     a >= [2, 1]                        # => true
+    #     a > [2, 1]                         # => false
+    #
+    def <=>(b)
+      return 0 if self == b
+      self.send :"compare_#{b.class}", b
+    end
+        
+    def >(other)  (self <=> other)  > 0 end
+    def <(other)  (self <=> other)  < 0 end
+    
+    def <=(other) (self <=> other) <= 0 end
+    def >=(other) (self <=> other) >= 0 end
+    
+    private
+    
+    def compare_Hash(b)
+      a = self
+      return -1 unless (a.values_at(*b.keys) & b.values).size >= b.size
+      1
+    end
+    alias_method :"compare_Selectable::Tags", :compare_Hash
+    
+    def compare_Array(b)
+      return -1 unless (self.values & b).size >= b.size
+      1
+    end
+    
+    def method_missing(meth, *args)
+      raise SelectableError, "#{args.first} is not a Hash or Array"
+    end
+    
+    ## NOTE: This is helpful but defensive. Ponder!
+    ##def compare_forced_array(b)
+    ##  compare_Array([b])
+    ##end
+    ##alias_method :compare_String, :compare_forced_array
+    ##alias_method :compare_Symbol, :compare_forced_array
+    ##alias_method :compare_Fixnum, :compare_forced_array
+      
+  end
+
+end
