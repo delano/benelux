a67a27408e365060aa8d23871e1d49b8d22d4aff
delano <delano@solutious.com>
2009-10-06 15:11:30 -0400

    Release 0.4.1 (reporter starts only one thread)

--- lib/benelux.rb
+++ lib/benelux.rb
@@ -5,7 +5,7 @@ require 'thwait'
 require 'selectable'
 
 module Benelux
-  VERSION = "0.4.0"
+  VERSION = "0.4.1"
   NOTSUPPORTED = [Class, Object, Kernel]
   
   class BeneluxError < RuntimeError; end
