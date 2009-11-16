c31125f956d9643e2827f812adbdeac8cd7b8730
delano <delano@solutious.com>
2009-11-16 18:55:35 -0500

    FIXED: Copy default tags when rotating timelines

--- lib/benelux/mixins/thread.rb
+++ lib/benelux/mixins/thread.rb
@@ -7,7 +7,9 @@ class Thread
   attic :rotated_timelines
   def rotate_timeline
     self.rotated_timelines << self.timeline
+    tags = self.timeline.default_tags.clone
     self.timeline = Benelux::Timeline.new
+    self.timeline.default_tags = tags
     self.timeline
   end
 end
